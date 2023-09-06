d2246d780b33cacd681404458c328b18a13d4dbc
Gero <concha.geronimojose@iesconcepcion.edu.ar>
2023-09-06 19:58:32 -0300

    Fin del proyecto

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-console.log('Estoy cargado...');
+document.addEventListener("submit",e=>{
+    e.preventDefault();
+    const num1 = document.querySelector("#num1").value;
+    const num2 = document.querySelector("#num2").value;
+    const resultado = Number(num1) + Number(num2)
+    
+    document.querySelector("#resultado").innerHTML = resultado
+})
