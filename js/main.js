7f487d96bfff1e2d2c1e9a4852f1c67185de1b39
AldiChegri <cecenarrochegri.aldanalucia@iesconcepcion.edu.ar>
2023-09-11 17:47:57 -0300

    Trabajo terminado

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-console.log('Estoy cargado...');
+document.addEventListener("submit",e=>{
+    e.preventDefault()
+    let num1 = document.querySelector("#num1").value
+    let num2 = document.querySelector("#num2").value
+    let suma = Number(num1) + Number(num2)
+    
+    document.querySelector("#resultado").innerHTML = suma
+})
