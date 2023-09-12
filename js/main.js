ae3d19c0f4fb719965db222b9d61c40942c7edb3
Luis945564 <lara.luis@iesconcepcion.edu.ar>
2023-09-12 16:56:45 -0300

    terminado

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-console.log('Estoy cargado...');
+const formulario = document.getElementById("formulario")
+formulario.addEventListener("submit", sumar)
+
+function sumar(event){
+    event.preventDefault()
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+    const resutaldo = parseInt(num1) + parseInt(num2);
+    document.getElementById("resultado").innerHTML = resutaldo;
+}
