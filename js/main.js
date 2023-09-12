b9fe883bc5af2be2a801996432d92817ca5c2afe
PaezCecilia <paez.andreacecilia@iesconcepcion.edu.ar>
2023-09-11 21:19:54 -0300

    Primer commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-console.log('Estoy cargado...');
+document.addEventListener("submit",e=>{
+    e.preventDefault()
+    let numero1 = document.querySelector("#num1").value
+    let numero2 = document.querySelector("#num2").value
+    let suma = Number(numero1) + Number(numero2)
+
+    document.querySelector("#resultado").innerHTML = suma
+})
